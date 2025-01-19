00d6162286729b02b270ccb6b90c252377c2f0ce
Jason <jasongms2017@gmail.com>
2025-01-19 01:17:10 -0500

    Finalized.

--- script.js
+++ script.js
@@ -25,7 +25,23 @@ var experiences_type = new Typed(".experiences-type", {
 var skills_type = new Typed(".skills-text", {
     strings: ["My Skills and Languages"],
     typeSpeed: 100,
-    backSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+})
+
+var projects_type = new Typed(".projects-type", {
+    strings: ["Projects"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+})
+
+var contacts_type = new Typed(".contacts-type", {
+    strings: ["Get in Contact"],
+    typeSpeed: 100,
+    backSpeed: 200,
     backDelay: 1000,
     loop: true
 })
