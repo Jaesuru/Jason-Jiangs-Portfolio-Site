5eb65a75f48ac1aa6eb7dd45c067a8f57404bc78
Jason <jasongms2017@gmail.com>
2025-01-18 19:38:15 -0500

    Bunch of new stuff.

---
+++ script.js
@@ -0,0 +1,31 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Software Developer.", "Web Developer.", "Game Developer."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var about_type = new Typed(".about-type", {
+    strings: ["About Me"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var experiences_type = new Typed(".experiences-type", {
+    strings: ["Experiences"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var skills_type = new Typed(".skills-text", {
+    strings: ["My Skills and Languages"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
