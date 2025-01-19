f8b85c581a8d5b07817e7d03d63e4dc1c799a740
Jason <jasongms2017@gmail.com>
2025-01-19 13:48:27 -0500

    updated

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 var typed = new Typed(".multiple-text", {
     strings: ["Software Developer.", "Web Developer.", "Game Developer."],
-    typeSpeed: 100,
-    backSpeed: 100,
+    typeSpeed: 50,
+    backSpeed: 25,
     backDelay: 1000,
     loop: true
 })
 
 var about_type = new Typed(".about-type", {
-    strings: ["About"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
+    strings: ["About Me"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
     loop: true
 })
 
 var experiences_type = new Typed(".experiences-type", {
     strings: ["Experiences"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 4000,
     loop: true
 })
 
 var skills_type = new Typed(".skills-text", {
-    strings: ["Skills"],
-    typeSpeed: 100,
-    backSpeed: 200,
+    strings: ["My Skills and Languages"],
+    typeSpeed: 50,
+    backSpeed: 25,
     backDelay: 1000,
     loop: true
 })
 
 var projects_type = new Typed(".projects-type", {
     strings: ["Projects"],
-    typeSpeed: 100,
-    backSpeed: 200,
-    backDelay: 1000,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
     loop: true
 })
